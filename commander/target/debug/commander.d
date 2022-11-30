a26cc991daf6c158531741850cb8698349a7c169
Sado Lin <sado.lin@gaia.net>
2022-11-30 13:43:54 +0800

    Revert "feat: add commander project"
    This reverts commit 048806cb79270594380d1393e2c66a88a9adb4ea.

--- commander/target/debug/commander.d
+++
@@ -1,1 +0,0 @@
-/home/sado/dev/rust/rust_practice/commander/target/debug/commander: /home/sado/dev/rust/rust_practice/commander/src/main.rs
--- commander/target/debug/deps/commander-766ba6c4b99c7630.d
+++
@@ -1,5 +0,0 @@
-/home/sado/dev/rust/rust_practice/commander/target/debug/deps/commander-766ba6c4b99c7630: src/main.rs
-
-/home/sado/dev/rust/rust_practice/commander/target/debug/deps/commander-766ba6c4b99c7630.d: src/main.rs
-
-src/main.rs:
